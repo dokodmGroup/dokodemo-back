5c2ff06a1c008cf8b992cbf1207a24276030b112
chenjiebin <sjlinyu@qq.com>
2015-01-01 23:53:34 +0800

    表单调整

--- application/models/Forms/User/Demo.php
+++ application/models/Forms/User/Demo.php
@@ -16,7 +16,7 @@ class DemoModel extends \Forms\AbstractModel {
         'demo1' => array(
             'label'    => 'demo1',
             'name'     => 'demo1',
-            'require'  => true,
+            'require'  => true, //表示字段是否必须
             "validate" => array(
                 array("type" => "int", "min" => "1", "max" => "3", "msg" => "demo1不正确")
             ),
@@ -24,12 +24,24 @@ class DemoModel extends \Forms\AbstractModel {
         'demo2' => array(
             'label'    => 'demo2',
             'name'     => 'demo2',
-            'require'  => false,
-            'default'  => "default value",
+            'require'  => false, //字段非必须
+            'default'  => "123456", //如果字段没有传入则设置该默认值，require为false时可以设置
             "validate" => array(
                 array("type" => "string", "min" => "6", "max" => "18", "msg" => "demo2不正确")
             ),
         ),
     );
+
+    /**
+     * 校验demo2字段
+     * 
+     * @return boolean
+     */
+    public function validateDemo2() {
+        $demo2 = $this->getFieldValue("demo2");
+        //这里可以进行更加复杂的校验
+
+        return true;
+    }
 
 }
