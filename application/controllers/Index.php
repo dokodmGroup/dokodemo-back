3b4f06cb9a88f960dc69eabe4f31cb1f9d87d1ea
chenjiebin <sjlinyu@qq.com>
2015-06-09 11:05:54 +0800

    删除测试代码

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -3,14 +3,7 @@
 class IndexController extends \Our\Controller_AbstractIndex {
 
     public function indexAction() {
-        $userRedis = \Redis\Db0\UserModel::getInstance();
-        $userRedis->getRedis();
-        $userRedis->update("1", array("hehe"));
-        var_dump($userRedis->find(1));
-
-//        sleep(3);
-//        
-//        var_dump($testRedis->find(1));
+        
     }
 
 }
