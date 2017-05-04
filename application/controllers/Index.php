50320c9593ee2dfeada7c24d174532373aa53c82
Tsukasa Kanzaki <tsukasa.kzk@gmail.com>
2017-05-04 17:10:02 +0800

    develop framework

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,9 +1,32 @@
 <?php
 
-class IndexController extends \Our\Controller_AbstractIndex {
+class IndexController extends \Our\Controller_AbstractApi {
+
+    public function init()
+    {
+        parent::init();
+        header('Content-Type: application/json');
+    }
 
     public function indexAction() {
-        
+        echo json_encode([
+            'key' => '嘿，你的燚龘！', 
+            'request_method' => $_SERVER['REQUEST_METHOD'],
+            'your_payload' => (array) json_decode(file_get_contents('php://input'), 1),
+            'your_query_string' => (array) $_GET,
+        ]);
+        http_response_code(202);
+    }
+
+    public function listAction() {
+        $dao = \DAO\TestModel::getInstance();
+        $result = $dao->fetchAll([], function($where){
+            $where->like('field1', '1')
+                ->or
+                ->like('field1', '0');
+        });
+        // echo json_encode($result);
+        $dao->debugDumpParams();
     }
 
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1'); // 显示错误信息
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET,POST,OPTIONS,PUT,DELETE');
+header('Access-Control-Allow-Headers: DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,X-Token');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
 date_default_timezone_set("Asia/Shanghai");
 mb_internal_encoding("UTF-8");
 define("APPLICATION_PATH", realpath(dirname(__FILE__) . '/../'));
