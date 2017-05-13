6ed23a7a8e50b2fc11da3e604b135cea0c13a9e9
Kanzaki Tsukasa <tsukasa.kzk@gmail.com>
2017-05-13 17:45:40 +0800

    unit framework fix

--- tests/application/library/Test/PHPUnit/TestCase.php
+++ tests/application/library/Test/PHPUnit/TestCase.php
@@ -2,7 +2,7 @@
 
 namespace Test\PHPUnit;
 
-class TestCase extends \PHPUnit_Framework_TestCase {
+class TestCase extends \PHPUnit\Framework\TestCase {
 
     /**
      * yaf运行实例
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,5 @@
 date_default_timezone_set("Asia/Shanghai");
 mb_internal_encoding("UTF-8");
 define("APPLICATION_PATH", realpath(dirname(__FILE__) . '/../'));
+
+require_once(APPLICATION_PATH . '/vendor/autoload.php');
