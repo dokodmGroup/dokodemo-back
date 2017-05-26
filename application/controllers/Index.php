ee9a82d390d436e1fd2a0eaa2238b30d3d4afe3a
Kanzaki Tsukasa <tsukasa.kzk@gmail.com>
2017-05-26 09:39:06 +0800

    Hey! your Yi Da!

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,21 +1,23 @@
 <?php
+use \TKS\ResponseHelper;
 
 class IndexController extends \Our\Controller_AbstractRest {
 
     public function init()
     {
         parent::init();
-        header('Content-Type: application/json');
     }
 
     public function indexAction() {
-        echo json_encode([
+        ResponseHelper::json(
+            202,
+            'success',
+            [
             'key' => '嘿，你的燚龘！', 
             'request_method' => $_SERVER['REQUEST_METHOD'],
             'your_payload' => (array) json_decode(file_get_contents('php://input'), 1),
             'your_query_string' => (array) $_GET,
         ]);
-        http_response_code(202);
     }
 
     public function listAction() {
@@ -25,8 +27,10 @@ class IndexController extends \Our\Controller_AbstractRest {
                 ->or
                 ->like('field1', '0');
         });
-        echo json_encode($result);
-        // $dao->debugDumpParams();
+        ResponseHelper::json(
+            202,
+            'success',
+            $result);
     }
 
 }
